f3ae143320081c4183b962580bc8b5e78c2bc25f
JWillmottHRGO <155462838+JWillmottHRGO@users.noreply.github.com>
2025-07-17 20:31:27 +0100

    Increased range of possible values of x in equation generator and added docsttring to equation generator function

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -1,8 +1,12 @@
 import random
 
 def generate_equation():
+    """
+    Generates two random integers and calculates their product.
+    Returns a tuple where the first item is an equation string with on integer replaced by 'x' and the second item is the value of x.
+    """
     given_factor = random.randint(1, 12)
-    missing_factor = random.randint(1, 2)
+    missing_factor = random.randint(1, 12)
     product = given_factor * missing_factor
 
     equation = f"{given_factor}x = {product}"
