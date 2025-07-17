5200b86e57be308794f10b6e2b4e715e89189a92
JWillmottHRGO <155462838+JWillmottHRGO@users.noreply.github.com>
2025-07-17 20:35:03 +0100

    Added functionality to track user's score and display score at the end of the program as a fraction and percentage

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from scripts.helper_functions import generate_equation
 
 question_total = 5
 question_count = 0
+score = 0
 
 while question_count < question_total:
 
@@ -13,9 +14,14 @@ while question_count < question_total:
     try:
         if int(user_response) == answer:
             print("Correct!")
+            score += 1
         else:
             print(f"Wrong, the correct answer is {answer}.")
     except ValueError as err:
         print(f"Invalid input. Please enter an integer. The correct answer is {answer}.")
     
     question_count += 1
+
+percentage = (score / question_total) * 100
+
+print(f"Test complete. Your score is {score}/{question_total}. That's {percentage}%")
